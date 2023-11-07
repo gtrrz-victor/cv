2bd4cc412624dffea107f0360d7451961b6574fe
gtrrz-victor <gtrrz.victor@gmail.com>
2023-11-07 12:27:57 +1100

    Change description, now based in Hobart.

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'GtrrZ | Victor Gutierrez Calderom Portfolio';
-export const SITE_DESCRIPTION = 'This is an interactive personal portfolio about Victor Gutierrez Calderon, a Fullstack Engineer based in Sydney.';
+export const SITE_DESCRIPTION = 'This is an interactive personal portfolio of Victor Gutierrez Calderon, a Fullstack Engineer based in Hobart, Tasmania."';
