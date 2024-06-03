e56300d73592673ecde721761ec13f9dbc77cbe6
Victor Gutierrez Calderon <victor@codezero.io>
2024-06-04 08:31:57 +1000

    fix: surname spelling typo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'GtrrZ | Victor Gutierrez Calderom Portfolio';
-export const SITE_DESCRIPTION = 'This is an interactive personal portfolio of Victor Gutierrez Calderon, a Fullstack Engineer based in Hobart, Tasmania."';
+export const SITE_TITLE = "GtrrZ | Victor Gutierrez Calderon Portfolio";
+export const SITE_DESCRIPTION =
+  'This is an interactive personal portfolio of Victor Gutierrez Calderon, a Fullstack Engineer based in Hobart, Tasmania."';
