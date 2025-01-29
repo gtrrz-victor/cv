f5dab802a8e3d510aee4d4342f3cad444eca6568
Victor Gutierrez Calderon <victor@codezero.io>
2025-01-30 09:23:28 +1100

    remove viewTransitions config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -8,15 +8,15 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cv.gtrrzvictor.com',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
+  site: "https://cv.gtrrzvictor.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
       cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )],
-  experimental: {
-    viewTransitions: true
-  }
+      logLevel: "debug",
+    }),
+  ],
 });
